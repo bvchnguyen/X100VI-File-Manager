b3b7de0fecde13827790bf69132ddf7c27afaad1
Bach Nguyen <bang4509@colorado.edu>
2024-07-07 17:02:59 -0600

    Confirmed move file status

--- main.py
+++ main.py
@@ -6,24 +6,48 @@ from manager.fileManager import fileManager
 
 def main():
 
-    fm = fileManager()
 
+    fm = fileManager()
+    fm.destination = '/Users/bvch/Library/CloudStorage/GoogleDrive-bvchnguyen@gmail.com'
     fm.path_to_drive("/Volumes/X100VI/DCIM/100_FUJI")
+
+    print(f"Google drive: {os.path.exists('/Users/bvch/Library/CloudStorage/GoogleDrive-bvchnguyen@gmail.com')}")
 
     if (fm.path == ""):
         print("Path does not exist")
         sys.exit(1)
 
+    # get user input
+    user_answer = True
+
+    while True:
+        user_answer = input("Do you want to create a new folder? (Y/N): ").strip().lower()
+        if user_answer == "y":
+            folder_name = input("Enter folder name: ")
+            fm.create_folder(fm.destination, folder_name)
+            fm.destination = os.path.join(fm.destination, folder_name)
+            break
+        elif user_answer == "n":
+            break
+        else:
+            print("Invalid input, please enter 'Y' or 'N'.")
+
+    print(f"New Destination: {fm.destination}")
     fm.get_files_by_type(".RAF")
+    fm.move_files_by_date_range("07-02-2024", fm.destination)
+    # fm.create_folder(fm.destination, "Odesza")
 
-    if (fm.fileList.__len__() == 0):
-        print("No RAF files found")
-        sys.exit(1)
+    # fm.get_files_by_type(".RAF")
 
-    fileTest = fm.get_file_by_index(0)
-    fm.get_path_to_file(fileTest)
-    print(fm.retrieve_file_metadata())
+    # if (fm.fileList.__len__() == 0):
+    #     print("No RAF files found")
+    #     sys.exit(1)
 
+    # fileTest = fm.get_file_by_index(0)
+    # fm.get_path_to_file(fileTest)
+    # print(fm.retrieve_file_metadata())
+
+    # fm.move_RAF_file(fileTest, "Odesza/RAF", False);
 
 if __name__ == "__main__":
     main()
--- manager/fileManager.py
+++ manager/fileManager.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+import shutil
 
 class fileManager():
     def __init__(self):
@@ -8,6 +9,9 @@ class fileManager():
         self.file: str
         self.fileList: list
         self.destination: str
+
+    def isPathValid(self, pathName: str) -> bool:
+        return os.path.exists(pathName)
 
     def path_to_drive(self, pathName: str) -> str:
         if(os.path.exists(pathName)):
@@ -22,6 +26,12 @@ class fileManager():
         self.fileList = [f for f in os.listdir(self.path) if f.endswith(fileType)]
         if self.fileList.__len__() != 0:
             return self.fileList
+            
+
+    def create_folder(self, rootPath: str, folderName: str) -> None:
+        if self.isPathValid(rootPath):
+            os.mkdir(os.path.join(rootPath, folderName))
+            print(f"Created folder: {folderName} in {rootPath}")
 
     def get_file_by_index(self, index: int) -> str:
         if index < self.fileList.__len__():
@@ -29,11 +39,16 @@ class fileManager():
             return self.file
         raise Exception(f"Index out of range")
         
-    def move_RAF_file():
+    def move_RAF_file(self, fileName: str, folderName: str):
+        if self.isPathValid(self.path) == False:
+            return "Path does not exist"
+        
+        if fileName.endswith(".RAF"):
+            shutil.move(self.path, os.path.join(self.destination, folderName, fileName))
+            print(f"Moved: {fileName} to {folderName}")
         return
     
     def retrieve_file_metadata(self):
-
         fileStats = os.stat(self.path)
 
         metadata = {
@@ -53,8 +68,28 @@ class fileManager():
         newform = format(size/1024, ".2f")
         return newform + " KB"
 
-    def get_files_by_date_range(self, startDate: str, endDate: str):
-        return
+    def move_files_by_date_range(self, targetDate: str, dest_path: str) -> None:
+        try: 
+            targetDate = datetime.datetime.strptime(targetDate, '%m-%d-%Y')
+        except ValueError:
+            print("Incorrect date format, should be MM-DD-YYYY")
+        
+        file_count = 0
+        for file in os.listdir(self.path):
+            print(file)
+            if(file.endswith(".RAF") == False):
+                continue
+            fileStats = os.stat(file)
+            fileDate = self.timeConvert(fileStats.st_ctime)
+            if fileDate >= targetDate:
+                file_count += 1
+                self.move_RAF_file(file, dest_path)
+                print(f"Moved: {file})")
+            if FileNotFoundError:
+                continue
+            if(file_count == 0):
+                print("No files found in date range")
+        print(f"Total files moved: {file_count}")
 
     def organize_file_by_date():
         return
